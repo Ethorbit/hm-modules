213b2f105268a4b5098e27bf7f6d0ed5e1778448
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-08-29 21:17:50 -0700

    Neovim, use virtual_text instead

--- modules/neovim/config/lua/options.lua
+++ modules/neovim/config/lua/options.lua
@@ -11,8 +11,8 @@ let g:transparent_enabled = 1
 -- 08/29/25 fix for messages not showing anymore after I updated neovim
 -- https://github.com/neovim/neovim/issues/33073
 vim.diagnostic.config({
-    virtual_lines = true,
-    --virtual_text = true,
+    virtual_text = true,
+    --virtual_lines = true,
 })
 
 ranger_replace_netrw = 1
