1e939bdf308fa8fa33ec4e1044e84c1ce1176032
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2025-09-23 20:41:16 -0700

    neovim, move package to packages.nix

--- modules/neovim/packages.nix
+++ modules/neovim/packages.nix
@@ -3,28 +3,39 @@
 with pkgs;
 
 {
-    programs.neovim.extraPackages = [
-        ranger
-        ripgrep
-        trash-cli
-        nil
-        unstable.mdx-language-server
-        (python3.withPackages (p: (with p; [
-            python-lsp-server
-            flake8
-            pycodestyle
-            pyflakes
-            pylint
-            pylsp-mypy
-            pyls-isort
-        ])))
-        nodePackages.eslint
-        vscode-langservers-extracted
-        nodePackages.typescript-language-server
-        nodePackages.svelte-language-server
-        lua-language-server
-        yaml-language-server
-        docker-compose-language-service
-        dockerfile-language-server-nodejs
-    ];
+    programs.neovim = {
+        # NixOS 25.05's neovim v0.11.3 broke built-in lua support
+        # I'm downgrading for the time being
+        package = lib.mkForce (
+            pkgs.old.neovim-unwrapped.overrideAttrs (old: {
+                meta = old.meta // { teams = []; };
+                buildInputs = (old.buildInputs or []) ++ [ ranger ];
+            })
+        );
+
+        extraPackages = [
+            ranger
+            ripgrep
+            trash-cli
+            nil
+            unstable.mdx-language-server
+            (python3.withPackages (p: (with p; [
+                python-lsp-server
+                flake8
+                pycodestyle
+                pyflakes
+                pylint
+                pylsp-mypy
+                pyls-isort
+            ])))
+            nodePackages.eslint
+            vscode-langservers-extracted
+            nodePackages.typescript-language-server
+            nodePackages.svelte-language-server
+            lua-language-server
+            yaml-language-server
+            docker-compose-language-service
+            dockerfile-language-server-nodejs
+        ];
+    };
 }
