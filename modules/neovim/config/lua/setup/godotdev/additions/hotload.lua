ae628ae3257e891ae4a6b2ea3d39c8048d06a878
Ethorbit <11908638+Ethorbit@users.noreply.github.com>
2026-06-04 17:07:51 -0700

    godotdev hotloading, suppress vim message

--- modules/neovim/config/lua/setup/godotdev/additions/hotload.lua
+++ modules/neovim/config/lua/setup/godotdev/additions/hotload.lua
@@ -12,8 +12,11 @@ vim.api.nvim_create_autocmd("BufWritePost", {
         -- Prevent duplicates
         vim.fn.system("pgrep -af 'godot' | grep -- '--path' | grep -v -- '--editor' | awk '{print $1}' | xargs -r kill")
 
+        local old_notify = vim.notify
+        vim.notify = function() end
         if not require("godotdev.run").run_current_scene() then
             require("godotdev.run").run_project()
         end
+        vim.notify = old_notify
     end
 })
